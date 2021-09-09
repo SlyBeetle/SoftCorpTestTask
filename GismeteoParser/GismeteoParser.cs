459c1414cf2036283c99fe06be70e9c5252bf76a
Andrei Palcheuski <andrew.palchevski@gmail.com>
2021-09-09 15:38:49 +0300

    refactor: extract GismeteoParser class

---
+++ GismeteoParser/GismeteoParser.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HtmlAgilityPack;
+using OpenQA.Selenium;
+
+namespace GismeteoParser
+{
+    internal class GismeteoParser
+    {
+        private const string HOME_PAGE = @"https://www.gismeteo.ru/";
+
+        private readonly IWebDriver _webDriver;
+
+        public GismeteoParser(IWebDriver webDriver)
+        {
+            _webDriver = webDriver;
+        }
+
+        public IEnumerable<string> GetUrlOfCities()
+        {
+            _webDriver.Navigate().GoToUrl(HOME_PAGE);
+            HtmlDocument homePage = new HtmlDocument();
+            homePage.LoadHtml(_webDriver.PageSource);
+            HtmlNodeCollection anchorsCollection = homePage.DocumentNode.SelectNodes("//section[@class=\"cities cities_frame __frame clearfix\"]//span[@class=\"cities_name\"]/..");
+            return anchorsCollection.Select(a => a.Attributes["href"].Value);
+        }
+    }
+}
--- GismeteoParser/Program.cs
+++ GismeteoParser/Program.cs
@@ -1,23 +1,14 @@
 ﻿using System;
-using System.Linq;
-using HtmlAgilityPack;
-using OpenQA.Selenium;
 using OpenQA.Selenium.PhantomJS;
 
 namespace GismeteoParser
 {
     internal class Program
     {
-        private const string HOME_PAGE = @"https://www.gismeteo.ru/";
-
         static void Main()
         {
-            IWebDriver driver = new PhantomJSDriver();
-            driver.Navigate().GoToUrl(HOME_PAGE);
-            HtmlDocument homePage = new HtmlDocument();
-            homePage.LoadHtml(driver.PageSource);
-            HtmlNodeCollection anchorsCollection = homePage.DocumentNode.SelectNodes("//section[@class=\"cities cities_frame __frame clearfix\"]//span[@class=\"cities_name\"]/..");
-            var urlOfCities = anchorsCollection.Select(a => a.Attributes["href"].Value);
+            GismeteoParser gismeteoParser = new GismeteoParser(new PhantomJSDriver());
+            var urlOfCities = gismeteoParser.GetUrlOfCities();
             Console.WriteLine();
             foreach (string url in urlOfCities)
             {
