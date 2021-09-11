9405bdcbec4157037aeab56b440ddbe1a100aec0
Andrei Palcheuski <andrew.palchevski@gmail.com>
2021-09-11 11:38:07 +0300

    refactor: remove unnecessary namespace

--- GismeteoParser/Services/ForecastFrameParser.cs
+++ GismeteoParser/Services/ForecastFrameParser.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using GismeteoParserConsoleApplication.Infrastructure;
 using GismeteoParserConsoleApplication.Models;
 using HtmlAgilityPack;
